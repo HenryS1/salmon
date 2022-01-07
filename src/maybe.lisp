64e1ddd8aaa87a99948a0de745003a6ec92dda5c
Henry Steere <henry.steere@gmail.com>
2022-01-07 20:16:36 +0200

    Use nil for empty value of maybe monad

--- src/maybe.lisp
+++ src/maybe.lisp
@@ -1,35 +1,25 @@
 (defpackage :maybe
   (:use :cl :monad)
   (:import-from :monad :fmap :flatmap)
-  (:export :value
-           :just
-           :nothing))
+  (:export :just-value
+           :just))
 
 (in-package :maybe)
 
-(defclass maybe () ())
+(defstruct just value)
 
-(defclass nothing (maybe)
-  ((value :accessor value :initform nil))) 
-
-(defclass just (maybe)
-  ((value :accessor value :initarg :value :initform (error "must initialize just with a value"))))
-
-(defvar *nothing* (make-instance 'nothing))
-
-(defun nothing () *nothing*)
-(defun just (v) (make-instance 'just :value v))
+(defun just (v) (make-just :value v))
 
 (defmethod print-object ((just just) out)
   (print-unreadable-object (just out :type t)
-    (format out "~a" (value just))))
+    (format out "~a" (just-value just))))
 
-(defmethod fmap (fun (nothing nothing)) nothing)
+(defmethod fmap (fun (v (eql nil))) v)
 
 (defmethod fmap (fun (just just))
-  (just (funcall fun (value just))))
+  (just (funcall fun (just-value just))))
 
-(defmethod flatmap (fun (nothing nothing)) nothing)
+(defmethod flatmap (fun (v (eql nil))) v)
 
 (defmethod flatmap (fun (just just))
-  (funcall fun (value just)))
+  (funcall fun (just-value just)))
