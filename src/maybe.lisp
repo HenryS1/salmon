a8b1ba696cd6653187cca12453e4405820d6c19d
Henry Steere <henrysteere@Henrys-MacBook-Pro.local>
2024-09-21 15:54:02 +0200

    Use :conc-name and :constructor to simplify

--- src/maybe.lisp
+++ src/maybe.lisp
@@ -1,25 +1,23 @@
 (defpackage :maybe
   (:use :cl :monad)
   (:import-from :monad :fmap :flatmap)
-  (:export :just-value
+  (:export :value
            :just))
 
 (in-package :maybe)
 
-(defstruct just value)
-
-(defun just (v) (make-just :value v))
+(defstruct (just (:constructor just (value)) (:conc-name)) value)
 
 (defmethod print-object ((just just) out)
   (print-unreadable-object (just out :type t)
-    (format out "~a" (just-value just))))
+    (format out "~a" (value just))))
 
 (defmethod fmap (fun (v (eql nil))) v)
 
 (defmethod fmap (fun (just just))
-  (just (funcall fun (just-value just))))
+  (just (funcall fun (value just))))
 
 (defmethod flatmap (fun (v (eql nil))) v)
 
 (defmethod flatmap (fun (just just))
-  (funcall fun (just-value just)))
+  (funcall fun (value just)))
--- tests/maybe-tests.lisp
+++ tests/maybe-tests.lisp
@@ -5,11 +5,11 @@
 
 (deftest just-test 
     (testing "just-value is inverse of just"
-      (ok (= (just-value (just 10)) 10))))
+      (ok (= (value (just 10)) 10))))
 
 (deftest fmaptest
     (testing "fmap action on just and nothing"
-      (ok (= (just-value (monad:fmap (lambda (x) (+ x 1)) (just 10)))
+      (ok (= (value (monad:fmap (lambda (x) (+ x 1)) (just 10)))
              11)
           "fmap applies a function to the value in just")
       (ok (eq (monad:fmap (lambda (x) (+ x 1)) nil) nil)
@@ -17,9 +17,9 @@
 
 (deftest flatmap-test
     (testing "flatmap action on just and nothing"
-      (ok (typep (just-value (monad:flatmap #'just (just 10))) 'integer)
+      (ok (typep (value (monad:flatmap #'just (just 10))) 'integer)
           "flatmap does not nest just")
-      (ok (= (just-value (monad:flatmap (lambda (x) (just (+ x 1))) (just 10))) 11)
+      (ok (= (value (monad:flatmap (lambda (x) (just (+ x 1))) (just 10))) 11)
           "flatmap applies a function to the value of just")
       (ok (eq (monad:flatmap (lambda (x) (+ x 1)) nil) nil)
           "flatmap leaves nil as nil")))
